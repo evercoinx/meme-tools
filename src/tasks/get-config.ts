4159194889b1b7aba66c1e0bf18186c0d9243676
Serge Grigorenko <surge@evercoin.xyz>
2025-03-08 19:53:16 +0100

    Refactor task to get config

--- src/tasks/get-config.ts
+++ src/tasks/get-config.ts
@@ -11,54 +11,55 @@ import { envVars, logger, OUTPUT_UNKNOWN_VALUE } from "../modules";
 
 (async () => {
     try {
-        logger.info(
-            "Configuration (%s):\n\t\tIPFS gateway URI: %s\n\t\tRPC URIs: %s\n\t\tExplorer URI: %s\n\n\t\tToken symbol: %s\n\t\tToken name: %s\n\t\tToken description: %s\n\t\tToken supply: %s\n\t\tToken tags: %s\n\t\tToken webiste URI: %s\n\t\tToken Twitter URI: %s\n\n\t\tPool size: %s\n\t\tPool liquidity: %s SOL\n\t\tPool trading cycle count: %s\n\t\tPool trading pump bias: %s\n\n\t\tSniper shares in pool: %s\n\t\tSniper balance: %s SOL\n\n\t\tTrader count: %s\n\t\tTrader group size: %s\n\t\tTrader balance: %s SOL\n\t\tTrader buy amount: %s SOL\n\t\tTrader sell amount: %s\n\t\tTrader swap delay: %s sec",
-            formatText(envVars.NODE_ENV, true),
-            formatUri(envVars.IPFS_GATEWAY_URI),
-            Array.from(envVars.RPC_URIS)
-                .map((rpcUri) => formatUri(new URL(rpcUri).origin))
-                .join(" "),
-            formatUri(envVars.EXPLORER_URI),
-            formatText(envVars.TOKEN_SYMBOL),
-            formatText(envVars.TOKEN_NAME),
-            formatText(envVars.TOKEN_DESCRIPTION),
-            formatInteger(envVars.TOKEN_SUPPLY),
-            Array.from(envVars.TOKEN_TAGS)
-                .map((tag) => formatText(tag))
-                .join(" "),
-            envVars.TOKEN_WEBSITE_URI ? formatUri(envVars.TOKEN_WEBSITE_URI) : OUTPUT_UNKNOWN_VALUE,
-            envVars.TOKEN_TWITTER_URI ? formatUri(envVars.TOKEN_TWITTER_URI) : OUTPUT_UNKNOWN_VALUE,
-            formatPercent(envVars.POOL_SIZE_PERCENT),
-            formatDecimal(envVars.POOL_LIQUIDITY_SOL),
-            formatInteger(envVars.POOL_TRADING_CYCLE_COUNT),
-            formatPercent(envVars.POOL_TRADING_PUMP_BIAS_PERCENT),
-            envVars.SNIPER_POOL_SHARE_PERCENTS.map((poolShare) => formatPercent(poolShare)).join(
-                " "
-            ),
-            formatDecimal(envVars.SNIPER_BALANCE_SOL),
-            formatInteger(envVars.TRADER_COUNT),
-            formatInteger(envVars.TRADER_GROUP_SIZE),
-            formatDecimal(envVars.TRADER_BALANCE_SOL),
-            [
-                formatDecimal(envVars.TRADER_BUY_AMOUNT_RANGE_SOL[0]),
-                " - ",
-                formatDecimal(envVars.TRADER_BUY_AMOUNT_RANGE_SOL[1]),
-            ].join(""),
-            [
-                formatPercent(envVars.TRADER_SELL_AMOUNT_RANGE_PERCENT[0]),
-                " - ",
-                formatPercent(envVars.TRADER_SELL_AMOUNT_RANGE_PERCENT[1]),
-            ].join(""),
-            [
-                formatMilliseconds(envVars.TRADER_SWAP_DELAY_RANGE_SEC[0]),
-                " - ",
-                formatMilliseconds(envVars.TRADER_SWAP_DELAY_RANGE_SEC[1]),
-            ].join("")
-        );
-
+        getConfig();
         process.exit(0);
     } catch (error: unknown) {
         logger.fatal(formatError(error));
         process.exit(1);
     }
 })();
+
+function getConfig(): void {
+    logger.info(
+        "Configuration (%s):\n\t\tIPFS gateway URI: %s\n\t\tRPC URIs: %s\n\t\tExplorer URI: %s\n\n\t\tToken symbol: %s\n\t\tToken name: %s\n\t\tToken description: %s\n\t\tToken supply: %s\n\t\tToken tags: %s\n\t\tToken webiste URI: %s\n\t\tToken Twitter URI: %s\n\n\t\tPool size: %s\n\t\tPool liquidity: %s SOL\n\t\tPool trading cycle count: %s\n\t\tPool trading pump bias: %s\n\n\t\tSniper shares in pool: %s\n\t\tSniper balance: %s SOL\n\n\t\tTrader count: %s\n\t\tTrader group size: %s\n\t\tTrader balance: %s SOL\n\t\tTrader buy amount: %s SOL\n\t\tTrader sell amount: %s\n\t\tTrader swap delay: %s sec",
+        formatText(envVars.NODE_ENV, true),
+        formatUri(envVars.IPFS_GATEWAY_URI),
+        Array.from(envVars.RPC_URIS)
+            .map((rpcUri) => formatUri(new URL(rpcUri).origin))
+            .join(" "),
+        formatUri(envVars.EXPLORER_URI),
+        formatText(envVars.TOKEN_SYMBOL),
+        formatText(envVars.TOKEN_NAME),
+        formatText(envVars.TOKEN_DESCRIPTION),
+        formatInteger(envVars.TOKEN_SUPPLY),
+        Array.from(envVars.TOKEN_TAGS)
+            .map((tag) => formatText(tag))
+            .join(" "),
+        envVars.TOKEN_WEBSITE_URI ? formatUri(envVars.TOKEN_WEBSITE_URI) : OUTPUT_UNKNOWN_VALUE,
+        envVars.TOKEN_TWITTER_URI ? formatUri(envVars.TOKEN_TWITTER_URI) : OUTPUT_UNKNOWN_VALUE,
+        formatPercent(envVars.POOL_SIZE_PERCENT),
+        formatDecimal(envVars.POOL_LIQUIDITY_SOL),
+        formatInteger(envVars.POOL_TRADING_CYCLE_COUNT),
+        formatPercent(envVars.POOL_TRADING_PUMP_BIAS_PERCENT),
+        envVars.SNIPER_POOL_SHARE_PERCENTS.map((poolShare) => formatPercent(poolShare)).join(" "),
+        formatDecimal(envVars.SNIPER_BALANCE_SOL),
+        formatInteger(envVars.TRADER_COUNT),
+        formatInteger(envVars.TRADER_GROUP_SIZE),
+        formatDecimal(envVars.TRADER_BALANCE_SOL),
+        [
+            formatDecimal(envVars.TRADER_BUY_AMOUNT_RANGE_SOL[0]),
+            " - ",
+            formatDecimal(envVars.TRADER_BUY_AMOUNT_RANGE_SOL[1]),
+        ].join(""),
+        [
+            formatPercent(envVars.TRADER_SELL_AMOUNT_RANGE_PERCENT[0]),
+            " - ",
+            formatPercent(envVars.TRADER_SELL_AMOUNT_RANGE_PERCENT[1]),
+        ].join(""),
+        [
+            formatMilliseconds(envVars.TRADER_SWAP_DELAY_RANGE_SEC[0]),
+            " - ",
+            formatMilliseconds(envVars.TRADER_SWAP_DELAY_RANGE_SEC[1]),
+        ].join("")
+    );
+}
