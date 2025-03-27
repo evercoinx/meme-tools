9d5fc55483461cd0a90a2fb1aa55ba87d3b78821
Surge <surge@evercoin.xyz>
2025-03-27 21:22:19 +0100

    Extend task to get config

--- src/tasks/get-config.ts
+++ src/tasks/get-config.ts
@@ -22,7 +22,7 @@ import { envVars, logger } from "../modules";
 
 function getConfig(): void {
     logger.info(
-        "Configuration (%s):\n\t\tIPFS gateway URI: %s\n\t\tRPC URIs: %s (Total: %s)\n\t\tExplorer URI: %s\n\n\t\tToken symbol: %s\n\t\tToken name: %s\n\t\tToken description: %s\n\t\tToken decimals: %s\n\t\tToken supply: %s\n\t\tToken webiste URI: %s\n\t\tToken Twitter URI: %s\n\t\tToken Telegram URI: %s\n\t\tToken tags: %s\n\n\t\tPool size: %s\n\t\tPool liquidity: %s SOL\n\t\tPool trading cycle count: %s\n\t\tPool trading pump bias: %s\n\n\t\tSniper shares in pool: %s (Total: %s)\n\t\tSniper balance: %s SOL\n\n\t\tTrader count: %s\n\t\tSwapper group size: %s\n\t\tTrader balance: %s SOL\n\t\tTrader buy amount: %s SOL\n\t\tTrader sell amount: %s\n\t\tSwapper trade delay: %s sec",
+        "Configuration (%s):\n\t\tIPFS gateway URI: %s\n\t\tRPC URIs: %s (Total: %s)\n\t\tExplorer URI: %s\n\n\t\tToken symbol: %s\n\t\tToken name: %s\n\t\tToken description: %s\n\t\tToken decimals: %s\n\t\tToken supply: %s\n\t\tToken webiste URI: %s\n\t\tToken Twitter URI: %s\n\t\tToken Telegram URI: %s\n\t\tToken tags: %s\n\n\t\tPool size: %s\n\t\tPool liquidity: %s SOL\n\t\tPool trading cycle count: %s\n\t\tPool trading pump bias: %s\n\n\t\tSniper shares in pool: %s (Total: %s)\n\t\tSniper balance: %s SOL\n\t\tSniper repeatable buy: %s SOL\n\t\tSniper repeatable sell: %s\n\t\tSniper repeatable buy amount: %s SOL\n\t\tSniper repeatable sell amount: %s\n\n\t\tTrader count: %s\n\t\tTrader balance: %s SOL\n\t\tTrader buy amount: %s SOL\n\t\tTrader sell amount: %s\n\n\t\tSwapper group size: %s\n\t\tSwapper trade delay: %s sec",
         formatText(envVars.NODE_ENV, true),
         formatUri(envVars.IPFS_GATEWAY_URI),
         Array.from(envVars.RPC_URIS)
@@ -50,8 +50,19 @@ function getConfig(): void {
             .join(" "),
         formatInteger(envVars.SNIPER_POOL_SHARE_PERCENTS.size),
         formatDecimal(envVars.SNIPER_BALANCE_SOL),
+        formatPercent(envVars.SNIPER_REPEATABLE_BUY_PERCENT),
+        formatPercent(envVars.SNIPER_REPEATABLE_SELL_PERCENT),
+        [
+            formatDecimal(envVars.SNIPER_REPEATABLE_BUY_AMOUNT_RANGE_SOL[0]),
+            " - ",
+            formatDecimal(envVars.SNIPER_REPEATABLE_BUY_AMOUNT_RANGE_SOL[1]),
+        ].join(""),
+        [
+            formatPercent(envVars.SNIPER_REPEATABLE_SELL_AMOUNT_RANGE_PERCENT[0]),
+            " - ",
+            formatPercent(envVars.SNIPER_REPEATABLE_SELL_AMOUNT_RANGE_PERCENT[1]),
+        ].join(""),
         formatInteger(envVars.TRADER_COUNT),
-        formatInteger(envVars.SWAPPER_GROUP_SIZE),
         formatDecimal(envVars.TRADER_BALANCE_SOL),
         [
             formatDecimal(envVars.TRADER_BUY_AMOUNT_RANGE_SOL[0]),
@@ -63,6 +74,7 @@ function getConfig(): void {
             " - ",
             formatPercent(envVars.TRADER_SELL_AMOUNT_RANGE_PERCENT[1]),
         ].join(""),
+        formatInteger(envVars.SWAPPER_GROUP_SIZE),
         [
             formatMilliseconds(envVars.SWAPPER_TRADE_DELAY_RANGE_SEC[0]),
             " - ",
